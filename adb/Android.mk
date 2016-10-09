718f5cbcb25580b909d821027af1c1175d6ef342
Adriano Martins <adrianomartins@gmail.com>
2016-10-08 22:19:40 -0300

    adb: disable all warnings being treated as errors
    Change-Id: If2cbce5ca425351115aa5f4acd71e40131b16fef

--- adb/Android.mk
+++ adb/Android.mk
@@ -71,7 +71,7 @@ else
   LOCAL_SRC_FILES += fdevent.c
 endif
 
-LOCAL_CFLAGS += -O2 -g -DADB_HOST=1 -Wno-unused-parameter -Wno-error
+LOCAL_CFLAGS += -O2 -g -DADB_HOST=1 -Wno-error
 LOCAL_CFLAGS += -D_XOPEN_SOURCE -D_GNU_SOURCE
 LOCAL_MODULE := adb
 LOCAL_MODULE_TAGS := debug
@@ -120,7 +120,7 @@ LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 endif
 
-LOCAL_CFLAGS := -O2 -g -DADB_HOST=0 -Wall -Wno-unused-parameter -Werror
+LOCAL_CFLAGS := -O2 -g -DADB_HOST=0 -Wno-error
 LOCAL_CFLAGS += -D_XOPEN_SOURCE -D_GNU_SOURCE
 
 ifneq (,$(filter userdebug eng,$(TARGET_BUILD_VARIANT)))
@@ -175,7 +175,7 @@ LOCAL_CFLAGS := \
 	-g \
 	-DADB_HOST=1 \
 	-DADB_HOST_ON_TARGET=1 \
-	-Wall -Wno-unused-parameter -Werror \
+	-Wno-error \
 	-D_XOPEN_SOURCE \
 	-D_GNU_SOURCE
 
